cf6315d1850d7149c69e1231e57e8fb840de955b
Farhan <ncem888@gmail.com>
2025-04-23 21:56:00 +0800

    add routes helper

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,28 @@ Route::get('/jobs/share', [JobController::class, 'share']);
 
 Route::resource('jobs', JobController::class);
 
+/******* Helper */
+Route::get('routes', function () {
+    // phpinfo();
+    $routeCollection = Route::getRoutes();
+
+    echo "<table style='width:100%'>";
+    echo '<tr>';
+    echo "<td width='10%'><h4>HTTP Method</h4></td>";
+    echo "<td width='10%'><h4>Route</h4></td>";
+    echo "<td width='10%'><h4>Name</h4></td>";
+    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
+    echo '</tr>';
+    foreach ($routeCollection as $value) {
+        echo '<tr>';
+        echo '<td>'.$value->methods()[0].'</td>';
+        echo '<td>'.$value->uri().'</td>';
+        echo '<td>'.$value->getName().'</td>';
+        echo '<td>'.$value->getActionName().'</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+});
 
 /******************************************** */
 
