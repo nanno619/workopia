2c09aca956ca80280032a5fddfd0b3ef7adc192e
Farhan <ncem888@gmail.com>
2025-04-30 11:14:37 +0800

    feat: :rocket: Header Component & URL helper

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,7 +3,12 @@
     <h1>Create New Job</h1>
     <form action="/jobs" method="POST">
         @csrf
-        <input type="text" name="title" id="title" placeholder="Title">
+        <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Title"
+        />
         <input type="text" name="description" id="description" placeholder="Description">
         <button type="submit">Submit</button>
     </form>
