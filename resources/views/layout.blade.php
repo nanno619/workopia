21a82f3e8488dc90999e9ae26c8cd96b7a3e0f8d
Farhan <ncem888@gmail.com>
2025-04-30 10:15:02 +0800

    feat: :rocket: Intro to components

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title', 'Workopia | Find & List Jobs')</title>
 </head>
 <body class="bg-gray-100">
-    @include('partials.navbar')
+    <x-header />
     <main class="container mx-auto p-4 mt-4">
         @yield('content')
     </main>
