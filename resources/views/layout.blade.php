ba79a96f9090cdfd2dcb619fd3f999eb65d85f6b
Farhan <ncem888@gmail.com>
2025-04-30 10:56:10 +0800

    feat: :rocket: Tailwind CSS & Vite Hot Reloading

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Workopia | Find & List Jobs' }}</title>
+    @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100">
     <x-header />
