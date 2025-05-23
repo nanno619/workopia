755131b7cba38095ec84e35e39b47635e81536d7
Farhan <ncem888@gmail.com>
2025-05-23 11:53:01 +0800

    :rocket:  Feat: Input Validation & Errors

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -3,23 +3,39 @@
     <h1>Create New Job</h1>
     <form action="/jobs" method="POST">
         @csrf
-        <label class="block text-gray-700" for="title">Job Title</label>
-        <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            class="w-full px-4 py-2 border rounded focus:outline-none"
-        />
+        <div class="my-5">
+            <label class="block text-gray-700" for="title">Job Title</label>
+            <input
+                type="text"
+                name="title"
+                placeholder="Title"
+                class="w-full px-4 py-2 border rounded focus:outline-none"
+                value="{{ old('title') }}"
+            />
+            @error('title')
+                <div class="text-red-500 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
 
-        <label class="block text-gray-700" for="title">Job Description</label>
-        <textarea
-            cols="30"
-            rows="7"
-            id="description"
-            name="description"
-            class="w-full px-4 py-2 border rounded focus:outline-none"
-            placeholder="We are seeking a skilled and motivated Software Developer to join our growing development team..."
-        ></textarea>
+        <div class="mb-3">
+            <label class="block text-gray-700" for="title">Job Description</label>
+            <textarea
+                cols="30"
+                rows="7"
+                id="description"
+                name="description"
+                class="w-full px-4 py-2 border rounded focus:outline-none"
+                placeholder="We are seeking a skilled and motivated Software Developer to join our growing development team..."
+            >{{ old('description') }}</textarea>
+            @error('description')
+                <div class="text-red-500 mt-2 text-sm">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+
         <button type="submit">Submit</button>
     </form>
 </x-layout>
