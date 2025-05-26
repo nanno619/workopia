c332e05e32fcb16e21ba714c8337140db489f4b2
Farhan <ncem888@gmail.com>
2025-05-26 14:19:58 +0800

    :rocket:  Feat: Homepage Jobs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Job;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('pages.index');
+        $jobs = Job::latest()->limit(6)->get();
+
+        $data = [
+            'jobs' => $jobs,
+        ];
+
+        return view('pages.index', $data);
     }
 }
