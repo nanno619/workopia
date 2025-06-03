0436499a39a55552d46bbedee167b9389a638c6c
Farhan <ncem888@gmail.com>
2025-06-03 10:29:51 +0800

    :rocket: Feat: Delete Job Listings

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -147,9 +147,16 @@ class JobController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): string
+    public function destroy(Job $job): RedirectResponse
     {
-        return 'Destroy';
+        // If logo, then delete it
+        if($job->company_logo){
+            Storage::disk('public')->delete($job->company_logo);
+        }
+
+        $job->delete();
+
+        return redirect()->route('jobs.index')->with('success', 'Job Listing deleted successfully!');
     }
 
     public function share(): string
