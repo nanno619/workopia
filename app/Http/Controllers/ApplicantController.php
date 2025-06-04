036b0e89155e0ecf12e955e04194a8d091bd41d8
Farhan <ncem888@gmail.com>
2025-06-04 16:32:48 +0800

    :rocket: Feat: Delete Applicants

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -40,4 +40,16 @@ class ApplicantController extends Controller
 
         return redirect()->back()->with('success', 'Your application has been submitted');
     }
+
+    /**
+     * @desc    Delete job application
+     * @route   DELETE /applicants/{applicant}
+     */
+    public function destroy($id): RedirectResponse
+    {
+        $applicant = Applicant::findOrFail($id);
+        $applicant->delete();
+
+        return redirect()->route('dashboard')->with('success', 'Applicant deleted successfully');
+    }
 }
