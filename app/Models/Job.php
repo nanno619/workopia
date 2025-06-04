eb3994bd38d3b4506b2e802e3470d6a389f0038f
Farhan <ncem888@gmail.com>
2025-06-04 12:18:12 +0800

    :rocket: Feat: Bookmarks Migration & Relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
@@ -39,5 +40,12 @@ class Job extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class); // Job belongs to User class
+    }
+
+    // Eg: $job->bookmarkedByUsers
+    // Relation to bookmark
+    public function bookmarkedByUsers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'job_user_bookmarks')->withTimestamps();
     }
 }
