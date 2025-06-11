682e4bfdb02c2eaee099cf1b6c2534ee7d77e92e
Farhan <ncem888@gmail.com>
2025-06-11 14:22:49 +0800

    :rocket: Feat: Sending data to emails

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -50,7 +50,7 @@ class ApplicantController extends Controller
         $application->save();
 
         // Send email to owner
-        Mail::to($job->user->email)->send(new JobApplied);
+        Mail::to($job->user->email)->send(new JobApplied($application, $job));
 
         return redirect()->back()->with('success', 'Your application has been submitted');
     }
