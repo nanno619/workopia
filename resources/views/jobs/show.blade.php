a5d1e86520bbe0a0e47ad69b44a91a0d5695462e
Farhan <ncem888@gmail.com>
2025-06-11 13:37:26 +0800

    :rocket: Mapbox setup

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -129,7 +129,7 @@
             </div>
 
             <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-                <div id="map"></div>
+                <div id="map" style="width:100%;height:400px;z-index:1;"></div>
             </div>
         </section>
 
@@ -183,3 +183,48 @@
         </aside>
     </div>
 </x-layout>
+
+
+<link href="https://api.mapbox.com/mapbox-gl-js/v3.12.0/mapbox-gl.css" rel="stylesheet">
+<script src="https://api.mapbox.com/mapbox-gl-js/v3.12.0/mapbox-gl.js"></script>
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    // Your Mapbox access token
+    mapboxgl.accessToken = "{{ env('MAPBOX_API_KEY') }}";
+
+    // Initialize the map
+    const map = new mapboxgl.Map({
+      container: 'map', // ID of the container element
+      style: 'mapbox://styles/mapbox/streets-v11', // Map style
+      center: [-74.5, 40], // Default center
+      zoom: 9, // Default zoom level
+    });
+
+    // Get address from Laravel view
+    const city = '{{ $job->city }}';
+    const state = '{{ $job->state }}';
+    const address = city + ', ' + state;
+
+    // Geocode the address
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+        address
+      )}.json?access_token=${mapboxgl.accessToken}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.features.length > 0) {
+          const [longitude, latitude] = data.features[0].center;
+
+          // Center the map and add a marker
+          map.setCenter([longitude, latitude]);
+          map.setZoom(14);
+
+          new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
+        } else {
+          console.error('No results found for the address.');
+        }
+      })
+      .catch((error) => console.error('Error geocoding address:', error));
+  });
+</script>
